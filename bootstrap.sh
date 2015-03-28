4561a08f2d399389fd6b44dc43e299289646c8a8
leturtle <turtle_ct@hotmail.com>
2015-03-28 21:55:50 +0800

    refactor bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,41 +1,64 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")"
-BACKUP_DIR="$HOME/.dotfiles.bak"
+BACKUP_DIR=$HOME/.dotfiles.bak
+
+cd $(dirname BASH_SOURCE)
+
 
 link_file () {
-  local dotfile=$1
-  local basename=$(basename "$dotfile")
-  if [ -f "$HOME/$basename" ]; then
-    if [ -h "$HOME/$basename" ]; then
-      rm "$HOME/$basename" 
-    else
-      backup_file "$basename"
-    fi
-  fi
+    local dotfile=$1
+    local linkfile=$2
 
-  ln -s "$dotfile" "$HOME/$basename"
-  echo "link $dotfile to $HOME/$basename"
+    if [ -f $linkfile ]; then
+        if [ -h $linkfile ]; then
+            echo remove link $linkfile
+            rm $linkfile
+        else
+            backup_file $linkfile
+        fi
+    fi
+
+    echo link $dotfile to $linkfile
+    ln -s $dotfile $linkfile
+}
+
+link_dir () {
+    local dotdir=$1
+    local linkdir=$HOME/$(basename $dotdir)
+    if [ ! -d $linkdir ]; then
+        echo create dir $linkdir
+        mkdir $linkdir
+    fi
+    for dotfile in $(ls -A $dotdir)
+    do
+        link_file $dotdir/$dotfile $HOME/$(basename $dotdir)/$dotfile
+    done
 }
 
 backup_file () {
-  local basename=$1
+    local file=$1
 
-  if [ ! -d "$BACKUP_DIR" ]; then
-    mkdir "$BACKUP_DIR"
-    echo "create $BACKUP_DIR"
-  fi
-  
-  mv "$HOME/$basename" "$BACKUP_DIR/$basename" 
-  echo "backup $HOME/$basename to $BACKUP_DIR"
+    if [ ! -d $BACKUP_DIR ]; then
+        echo create $BACKUP_DIR
+        mkdir $BACKUP_DIR
+    fi
+
+    echo backup $file to $BACKUP_DIR
+    mv $file $BACKUP_DIR
 }
 
-for dotfile in $(find "$(pwd)" -depth 2 -name '.*')
+for dotfile in $(find $(pwd) -depth 2 -name ".*")
 do
-  link_file "$dotfile" 
+    if [ -d $dotfile ]; then
+        link_dir $dotfile
+    else
+        link_file $dotfile $HOME/$(basename $dotfile)
+    fi
 done
 
-for initscript in $(find "$(pwd)" -depth 2 -name "init.*")
+if [[ $1 && ( $1 == "init") ]]; then
+for initscript in $(find $(pwd) -depth 2 -name "init.*")
 do
-  /usr/bin/env osascript "$initscript"  
+    /usr/bin/env osascript "$initscript"
 done
+fi
